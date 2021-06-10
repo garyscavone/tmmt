12ecd8326b6f56a91be9fda3622685d458ac545f
garyscavone <garyscavone@users.noreply.github.com>
2021-06-10 15:19:26 -0400

    Added ASA2021 tutorial material

---
+++ asa2021/internals.pl
@@ -0,0 +1,142 @@
+# LaTeX2HTML 2021 (Released January 1, 2021)
+# Associate internals original text with physical files.
+
+
+$key = q/cite_chaigne:acoustics/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dalmont:flanges/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dalmont:sidehole/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dubos:sidehole/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eveno:hornradiation/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_keefe:models/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_keefe:thesis/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kergomard:interactions/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lefebvre:aaua2013/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lefebvre:jasafem/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_levine:pipe/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nederveen:sidehole/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_norris:flange/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_plitnik:oboe/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wang:jasa2020/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wang:jasa2021/;
+$ref_files{$key} = "$dir".q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:cylrefl/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:evenw/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:oddw/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:zin/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:zinab/;
+$ref_files{$key} = "$dir".q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:newton/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:symtransmatrix/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:toneholematrix/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zac/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zin/;
+$ref_files{$key} = "$dir".q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zsc/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zso/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:chpt1:coneapprox/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:chpt1:cylinder/;
+$ref_files{$key} = "$dir".q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cone/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:coneapprox/;
+$ref_files{$key} = "$dir".q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cylapprox/;
+$ref_files{$key} = "$dir".q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:toneholeTrans/;
+$ref_files{$key} = "$dir".q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
---
+++ asa2021/labels.pl
@@ -0,0 +1,193 @@
+# LaTeX2HTML 2021 (Released January 1, 2021)
+# Associate labels original text with physical files.
+
+
+$key = q/cite_chaigne:acoustics/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dalmont:flanges/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dalmont:sidehole/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_dubos:sidehole/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_eveno:hornradiation/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_keefe:models/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_keefe:thesis/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_kergomard:interactions/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lefebvre:aaua2013/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_lefebvre:jasafem/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_levine:pipe/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_nederveen:sidehole/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_norris:flange/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_plitnik:oboe/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wang:jasa2020/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/cite_wang:jasa2021/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:cylrefl/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:evenw/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:oddw/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:zin/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:chpt1:zinab/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:newton/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:symtransmatrix/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:toneholematrix/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zac/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zin/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zsc/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zso/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:chpt1:coneapprox/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:chpt1:cylinder/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cone/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:coneapprox/;
+$external_labels{$key} = "$URL/" . q|node6.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cylapprox/;
+$external_labels{$key} = "$URL/" . q|node3.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:toneholeTrans/;
+$external_labels{$key} = "$URL/" . q|node7.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2021 (Released January 1, 2021)
+# labels from external_latex_labels array.
+
+
+$key = q/eq:newton/;
+$external_latex_labels{$key} = q|2 A Finite Length Cylindrical Pipe|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:symtransmatrix/;
+$external_latex_labels{$key} = q|16 Side Branches or Toneholes|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:toneholematrix/;
+$external_latex_labels{$key} = q|17 Side Branches or Toneholes|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zac/;
+$external_latex_labels{$key} = q|21 Side Branches or Toneholes|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zin/;
+$external_latex_labels{$key} = q|5 A Finite Length Cylindrical Pipe|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zsc/;
+$external_latex_labels{$key} = q|19 Side Branches or Toneholes|; 
+$noresave{$key} = "$nosave";
+
+$key = q/eq:zso/;
+$external_latex_labels{$key} = q|18 Side Branches or Toneholes|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cone/;
+$external_latex_labels{$key} = q|7 A conical segment|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:coneapprox/;
+$external_latex_labels{$key} = q|8 A non-uniform bore (a) and its approximation in terms of cylindrical and conical sections (b)|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:cylapprox/;
+$external_latex_labels{$key} = q|3 A non-uniform bore (a) and its approximation in terms of cylindrical sections (b)|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig:toneholeTrans/;
+$external_latex_labels{$key} = q|11 $T$ section transmission-line representation of the tonehole|; 
+$noresave{$key} = "$nosave";
+
+1;
+
